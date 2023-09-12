9da3b9f73d047fbdecfd8f8bab1b5e1f36e9cec7
Patrick Muriungi <muriungipatrick@gmail.com>
2023-09-12 09:02:30 +0300

    First commit and setup

---
+++ src/pages/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <h4>Bavbar</h4>
+    </a>
+  </div>
+</nav>
+    );
+};
+
+
+export default Header;
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+    <div className="bg-primary text-center text-capitalize">
+        <h1>Login form</h1>
+        
+    </div>
+    );
+};
+
+
+export default Login;
